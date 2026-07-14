d989bd2f9aa6938d0a691da61d6f4e3d99bec00c
amittechnichal <amit089.sharma@gmail.com>
2026-07-14 15:17:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "amit-demo-bucket-4789"
-  location = "ASIA-SOUTH2"
+  name                        = "amit-demo-bucket-4789-001"
+  location                    = "ASIA-SOUTH2"
+  uniform_bucket_level_access = true
 }
